3133397a5d0c6f361b6e6d3fc123eb0235094e94
javierbasualdo <javierbasualdocom@gmail.com>
2023-02-03 17:51:12 -0300

    svelte game

---
+++ svelte/src/components/Cell.svelte
@@ -0,0 +1,42 @@
+<script>
+import { createEventDispatcher } from 'svelte';
+export let datacell;
+export let piece;
+export let win;
+
+const dispatch = createEventDispatcher();
+
+function send(){
+    dispatch('clicked');
+}
+
+$: cellWinner = win.includes(datacell) ? 'cell-winner' : '';
+
+</script>
+
+<div class={"cell " + cellWinner} on:click|once={send}>
+    <span>{piece}</span>
+</div>
+
+<style>
+.cell {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    cursor: pointer;
+}
+.cell:hover {
+    background-color: rgb(55, 51, 51);
+}
+.cell span {
+    font-size: 7em;
+}
+.cell span::after {
+    content: ' ';
+    display: inline-block;
+}
+.cell-winner {
+    background-color: gold;
+}
+</style>
---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+import Desk from '../components/Desk.svelte';
+
+let unique = {};
+
+function changeKey() {
+    unique = {};
+}
+</script>
+
+<main class="container">
+    {#key unique}
+    <Desk on:reload={changeKey}/>
+    {/key}
+</main>
+
+<style>
+.container {
+    display: grid;
+    height: 90vh;
+    aspect-ratio: 1;
+}
+</style>
